00c04a7c6595c1c7ce0b7e677c873354ff71d7ca
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-01-12 15:10:32 +0100

    improved QDLLEXPORT macro

--- include/uibase/dllimport.h
+++ include/uibase/dllimport.h
@@ -24,18 +24,20 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 namespace MOBase
 {
 
-#ifdef _WIN32
-#if defined(UIBASE_EXPORT)
+// source: https://gcc.gnu.org/wiki/Visibility
+#if defined _WIN32 || defined __CYGWIN__
+#ifdef UIBASE_EXPORT
 #define QDLLEXPORT __declspec(dllexport)
-#elif defined(_NODLL)
-#define QDLLEXPORT
 #else
-#undef DLLEXPORT
 #define QDLLEXPORT __declspec(dllimport)
 #endif
-#else  // _WIN32
+#else  // defined _WIN32 || defined __CYGWIN__
+#if __GNUC__ >= 4
 #define QDLLEXPORT __attribute__((visibility("default")))
-#endif  // _WIN32
+#else
+#define QDLLEXPORT
+#endif
+#endif  // defined _WIN32 || defined __CYGWIN__
 
 }  // namespace MOBase
 
