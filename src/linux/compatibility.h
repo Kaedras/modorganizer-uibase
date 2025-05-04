142b468aacbcc7bf6b6d852505ee683c86a3d3cd
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-05-05 01:33:53 +0200

    added some more function wrappers to linux/compatibility.h, copied documentation from standard libraries

--- src/linux/compatibility.h
+++ src/linux/compatibility.h
@@ -3,6 +3,11 @@
 #include "../dllimport.h"
 #include <cstdint>
 #include <unistd.h>
+#include <poll.h>
+extern "C"
+{
+#include <sys/pidfd.h>
+}
 
 // windows types
 
@@ -43,11 +48,67 @@ static inline constexpr int INVALID_HANDLE_VALUE = -1;
 #define ERROR_BAD_ARGUMENTS EINVAL
 #define ERROR_CANCELLED ECANCELED
 
-// functions
+// windows function wrappers
 
 inline int GetLastError()
 {
   return errno;
+}
+
+inline void SetLastError(int error)
+{
+  errno = error;
+}
+
+/* Get the process ID of the calling process.  */
+inline DWORD GetCurrentProcessId()
+{
+  return getpid();
+}
+
+/* Returns a file descriptor that refers to the process PID.  The
+   close-on-exec is set on the file descriptor.  */
+inline HANDLE GetCurrentProcess()
+{
+  return pidfd_open(getpid(), 0);
+}
+
+/* Query the process ID (PID) from process descriptor FD.  Return the PID
+   or -1 in case of an error.  */
+inline DWORD GetProcessId(HANDLE fd)
+{
+  return pidfd_getpid(fd);
+}
+
+/* Close the file descriptor FD.
+
+   This function is a cancellation point and therefore not marked with
+   __THROW.  */
+inline bool CloseHandle(HANDLE fd)
+{
+  return close(fd) == 0;
+}
+
+/* Close the file descriptor FD.
+
+   This function is a cancellation point and therefore not marked with
+   __THROW.  */
+inline int NtClose(HANDLE fd)
+{
+  return close(fd);
+}
+
+/* Poll the file descriptors. If TIMEOUT is nonzero and not -1, allow TIMEOUT milliseconds for
+   an event to occur; if TIMEOUT is -1, block until an event occurs.
+   Returns the number of file descriptors with events, zero if timed out,
+   or -1 for errors.
+
+   This function is a cancellation point and therefore not marked with
+   __THROW.  */
+inline int WaitForSingleObject(HANDLE fd, int timeout)
+{
+  pollfd pfd = {fd, POLLIN, 0};
+  return poll(&pfd, 1, timeout);
 }
 
 #define sprintf_s sprintf
