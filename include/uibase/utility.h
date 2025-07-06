9c64ebfae89c1ea6285df1d4faa6e981b7b4f629
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-07-07 01:21:51 +0200

    add fileTimeToQDateTime function

--- include/uibase/utility.h
+++ include/uibase/utility.h
@@ -391,6 +391,15 @@ QDLLEXPORT QString ToQString(const std::wstring& source);
  **/
 QDLLEXPORT QString ToString(const SYSTEMTIME& time);
 
+/**
+ * @brief converts FILETIME to QDateTime
+ * @param fileTime the time to convert
+ * @param timeZone timezone to use. Examples: QTimeZone::systemTimeZone(),
+ * QTimeZone::utc()
+ */
+QDLLEXPORT QDateTime fileTimeToQDateTime(const FILETIME& fileTime,
+                                         const QTimeZone& timeZone = QTimeZone::utc());
+
 // three-way compare for natural sorting (case insensitive default, 10 comes
 // after 2)
 //
--- src/utility.cpp
+++ src/utility.cpp
@@ -463,6 +463,18 @@ QString ToQString(const std::wstring& source)
   return QString::fromStdWString(source);
 }
 
+QDateTime fileTimeToQDateTime(const FILETIME& fileTime, const QTimeZone& timeZone)
+{
+  static constexpr int64_t WINDOWS_TICK      = 10000000;
+  static constexpr int64_t SEC_TO_UNIX_EPOCH = 11644473600LL;
+
+  int64_t timeInt =
+      static_cast<int64_t>(fileTime.dwHighDateTime) << 32 | fileTime.dwLowDateTime;
+  time_t unixTime = timeInt / WINDOWS_TICK - SEC_TO_UNIX_EPOCH;
+
+  return QDateTime::fromSecsSinceEpoch(unixTime, timeZone);
+}
+
 static int naturalCompareI(const QString& a, const QString& b)
 {
   static QCollator c = [] {
