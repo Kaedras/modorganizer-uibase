0619aff266d57dfc168a822c1de65f286643f60a
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-07-07 01:33:47 +0200

    add additional functions to FdCloser
    function signatures were copied from std::unique_ptr

--- include/uibase/linux/fdcloser.h
+++ include/uibase/linux/fdcloser.h
@@ -1,23 +1,32 @@
 #pragma once
 
+#include "../dllimport.h"
+
+#include <cstddef>
+
 class QDLLEXPORT FdCloser
 {
 public:
-  FdCloser();
-  FdCloser(int fd);
+  FdCloser() noexcept;
+  FdCloser(std::nullptr_t) noexcept;
+  FdCloser(int fd) noexcept;
+
+  FdCloser(FdCloser&&)      = default;
+  FdCloser(const FdCloser&) = delete;
 
   ~FdCloser();
 
-  FdCloser& operator=(int fd);
+  FdCloser& operator=(FdCloser&& other) noexcept;
+  FdCloser& operator=(int fd) noexcept;
+  int operator->() const noexcept;
+  int operator*() const noexcept;
 
   operator bool() const noexcept;
 
-  int get() const;
-  void reset(int value);
-
-  int release();
-
-  bool isValid() const;
+  int get() const noexcept;
+  void reset(int fd) noexcept;
+  int release() noexcept;
+  bool isValid() const noexcept;
 
 private:
   int m_fd;
--- src/linux/fdcloser.cpp
+++ src/linux/fdcloser.cpp
@@ -1,17 +1,28 @@
 #include "linux/fdcloser.h"
 
-FdCloser::FdCloser() : m_fd(-1) {}
+FdCloser::FdCloser() noexcept : m_fd(-1) {}
 
-FdCloser::FdCloser(int fd) : m_fd(fd) {}
+FdCloser::FdCloser(std::nullptr_t) noexcept : m_fd(-1) {}
 
-FdCloser::~FdCloser()
+FdCloser::FdCloser(int fd) noexcept : m_fd(fd) {}
+
+FdCloser& FdCloser::operator=(FdCloser&& other) noexcept
+{
+  if (m_fd != -1) {
+    close(m_fd);
+  }
+  m_fd = other.m_fd;
+  return *this;
+}
+
+FdCloser::~FdCloser() noexcept
 {
   if (m_fd != -1) {
     close(m_fd);
   }
 }
 
-FdCloser& FdCloser::operator=(int fd)
+FdCloser& FdCloser::operator=(int fd) noexcept
 {
   if (m_fd != -1) {
     close(m_fd);
@@ -23,27 +34,40 @@ FdCloser& FdCloser::operator=(int fd)
 
 FdCloser::operator bool() const noexcept
 {
-  return m_fd != -1;
+  return isValid();
 }
 
-int FdCloser::get() const
+int FdCloser::operator->() const noexcept
 {
   return m_fd;
 }
 
-void FdCloser::reset(int value)
+int FdCloser::operator*() const noexcept
 {
+  return m_fd;
+}
+
+int FdCloser::get() const noexcept
+{
+  return m_fd;
+}
+
+void FdCloser::reset(int value) noexcept
+{
+  if (isValid()) {
+    close(m_fd);
+  }
   m_fd = value;
 }
 
-int FdCloser::release()
+int FdCloser::release() noexcept
 {
   int tmp = m_fd;
   m_fd    = -1;
   return tmp;
 }
 
-bool FdCloser::isValid() const
+bool FdCloser::isValid() const noexcept
 {
   return m_fd != -1;
 }
