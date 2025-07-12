19751cb03bda5c0bd4f4c71fc46bb9f0e93c94da
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-07-12 14:06:20 +0200

    update translations

--- src/uibase_en.ts
+++ src/uibase_en.ts
@@ -96,16 +96,63 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="linux/report_linux.cpp" line="19"/>
-        <location filename="win32/report_win32.cpp" line="20"/>
-        <location filename="win32/report_win32.cpp" line="26"/>
-        <source>Error</source>
+        <location filename="linux/registry.cpp" line="61"/>
+        <location filename="linux/registry.cpp" line="62"/>
+        <location filename="textviewer.cpp" line="175"/>
+        <location filename="textviewer.cpp" line="176"/>
+        <location filename="win32/registry.cpp" line="52"/>
+        <location filename="win32/registry.cpp" line="53"/>
+        <location filename="win32/registry.cpp" line="115"/>
+        <location filename="win32/registry.cpp" line="116"/>
+        <source>INI file is read-only</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="win32/utility_win32.cpp" line="314"/>
-        <location filename="win32/utility_win32.cpp" line="337"/>
-        <source>You have an invalid custom browser command in the settings.</source>
+        <location filename="linux/registry.cpp" line="63"/>
+        <location filename="textviewer.cpp" line="177"/>
+        <location filename="win32/registry.cpp" line="54"/>
+        <location filename="win32/registry.cpp" line="117"/>
+        <source>Mod Organizer is attempting to write to &quot;%1&quot; which is currently set to read-only.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="linux/registry.cpp" line="67"/>
+        <location filename="textviewer.cpp" line="181"/>
+        <location filename="win32/registry.cpp" line="58"/>
+        <location filename="win32/registry.cpp" line="121"/>
+        <source>Clear the read-only flag</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="linux/registry.cpp" line="68"/>
+        <location filename="textviewer.cpp" line="182"/>
+        <location filename="win32/registry.cpp" line="59"/>
+        <location filename="win32/registry.cpp" line="122"/>
+        <source>Allow the write once</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="linux/registry.cpp" line="69"/>
+        <location filename="textviewer.cpp" line="183"/>
+        <location filename="win32/registry.cpp" line="60"/>
+        <location filename="win32/registry.cpp" line="123"/>
+        <source>The file will be set to read-only again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="linux/registry.cpp" line="71"/>
+        <location filename="textviewer.cpp" line="185"/>
+        <location filename="win32/registry.cpp" line="62"/>
+        <location filename="win32/registry.cpp" line="125"/>
+        <source>Skip this file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="linux/report_linux.cpp" line="19"/>
+        <location filename="linux/report_linux.cpp" line="24"/>
+        <location filename="win32/report_win32.cpp" line="20"/>
+        <location filename="win32/report_win32.cpp" line="26"/>
+        <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -125,44 +172,6 @@
             <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
-    </message>
-    <message>
-        <location filename="registry.cpp" line="52"/>
-        <location filename="registry.cpp" line="53"/>
-        <location filename="textviewer.cpp" line="175"/>
-        <location filename="textviewer.cpp" line="176"/>
-        <source>INI file is read-only</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="registry.cpp" line="54"/>
-        <location filename="textviewer.cpp" line="177"/>
-        <source>Mod Organizer is attempting to write to &quot;%1&quot; which is currently set to read-only.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="registry.cpp" line="58"/>
-        <location filename="textviewer.cpp" line="181"/>
-        <source>Clear the read-only flag</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="registry.cpp" line="59"/>
-        <location filename="textviewer.cpp" line="182"/>
-        <source>Allow the write once</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="registry.cpp" line="60"/>
-        <location filename="textviewer.cpp" line="183"/>
-        <source>The file will be set to read-only again.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="registry.cpp" line="62"/>
-        <location filename="textviewer.cpp" line="185"/>
-        <source>Skip this file</source>
-        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="report.cpp" line="323"/>
@@ -186,90 +195,104 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="safewritefile.cpp" line="43"/>
+        <location filename="safewritefile.cpp" line="44"/>
         <source>Failed to save &apos;%1&apos;, could not create a temporary file: %2 (error %3)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="109"/>
+        <location filename="utility.cpp" line="69"/>
+        <location filename="win32/utility_win32.cpp" line="64"/>
         <source>removal of &quot;%1&quot; failed: %2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="118"/>
+        <location filename="utility.cpp" line="78"/>
+        <location filename="win32/utility_win32.cpp" line="73"/>
         <source>removal of &quot;%1&quot; failed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="122"/>
+        <location filename="utility.cpp" line="82"/>
+        <location filename="win32/utility_win32.cpp" line="77"/>
         <source>&quot;%1&quot; doesn&apos;t exist (remove)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="218"/>
+        <location filename="utility.cpp" line="178"/>
+        <location filename="win32/utility_win32.cpp" line="352"/>
         <source>Error %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="371"/>
-        <location filename="utility.cpp" line="400"/>
+        <location filename="utility.cpp" line="289"/>
+        <location filename="utility.cpp" line="318"/>
+        <location filename="win32/utility_win32.cpp" line="678"/>
+        <location filename="win32/utility_win32.cpp" line="707"/>
         <source>failed to create directory &quot;%1&quot;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="380"/>
-        <location filename="utility.cpp" line="407"/>
+        <location filename="utility.cpp" line="298"/>
+        <location filename="utility.cpp" line="325"/>
+        <location filename="win32/utility_win32.cpp" line="687"/>
+        <location filename="win32/utility_win32.cpp" line="714"/>
         <source>failed to copy &quot;%1&quot; to &quot;%2&quot;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="710"/>
+        <location filename="utility.cpp" line="628"/>
         <source>%1 B</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="710"/>
+        <location filename="utility.cpp" line="628"/>
         <source>%1 KB</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="711"/>
+        <location filename="utility.cpp" line="629"/>
         <source>%1 MB</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="711"/>
+        <location filename="utility.cpp" line="629"/>
         <source>%1 GB</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="712"/>
+        <location filename="utility.cpp" line="630"/>
         <source>%1 TB</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="717"/>
+        <location filename="utility.cpp" line="635"/>
         <source>%1 B/s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="717"/>
+        <location filename="utility.cpp" line="635"/>
         <source>%1 KB/s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="718"/>
+        <location filename="utility.cpp" line="636"/>
         <source>%1 MB/s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="718"/>
+        <location filename="utility.cpp" line="636"/>
         <source>%1 GB/s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="719"/>
+        <location filename="utility.cpp" line="637"/>
         <source>%1 TB/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="win32/utility_win32.cpp" line="537"/>
+        <location filename="win32/utility_win32.cpp" line="560"/>
+        <source>You have an invalid custom browser command in the settings.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -386,19 +409,19 @@
 <context>
     <name>uibase</name>
     <message>
-        <location filename="utility.cpp" line="781"/>
+        <location filename="utility.cpp" line="698"/>
         <source>h</source>
         <extracomment>Time remaining hours</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="784"/>
+        <location filename="utility.cpp" line="701"/>
         <source>m</source>
         <extracomment>Time remaining minutes</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="utility.cpp" line="787"/>
+        <location filename="utility.cpp" line="704"/>
         <source>s</source>
         <extracomment>Time remaining seconds</extracomment>
         <translation type="unfinished"></translation>
